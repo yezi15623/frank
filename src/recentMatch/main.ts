f0cd5d00183ed15ac1450862a955e4d02185081b
yezi15623 <75667873+yezi15623@users.noreply.github.com>
2026-06-24 14:19:56 +0800

    docs: 为游戏内对局窗口入口添加中文注释

--- src/recentMatch/main.ts
+++ src/recentMatch/main.ts
@@ -1,6 +1,8 @@
-import './style.css';
+import "./style.css";
 // @ts-ignore
-import App from './main.vue';
-import { createApp } from 'vue';
+import App from "./main.vue";
+import { createApp } from "vue";
 
-createApp(App).mount('#app')
+// recentMatchWindow 的 Vue 入口。
+// 该窗口由 background/gameFlow.ts 在游戏开始后按配置创建，主要用于游戏内展示双方近期战绩。
+createApp(App).mount("#app");
